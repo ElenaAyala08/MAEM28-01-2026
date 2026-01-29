4b2212ed3820d0a0f317c8f55ed5334f16a217e5
Maria Elena Ayala Martinez <mariaelenaayala10@outlook.com>
2026-01-29 13:09:03 -0600

    configuración conexión mongodb

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB conectado');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = connectDB;
